c2e806894a982bf0f648129229bceacdb682da52
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2025-05-04 13:48:32 +0200

    Initial commit

---
+++ src/client/java/io/github/thijzert123/mymodsmychoice/MyModsMyChoiceClient.java
@@ -0,0 +1,10 @@
+package io.github.thijzert123.mymodsmychoice;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyModsMyChoiceClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
